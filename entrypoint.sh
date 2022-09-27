776f5d86b489ae5055dd9713ba11f64d140017e4
binarstrike <binarnugroho775@gmail.com>
2022-09-27 09:33:22 +0000

    update: blm selesai

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,26 @@
 if [ -z $1 ]; then
-	echo "error empty parameter"
-	echo "SERVER_ID APPLICATION_ID TOKEN"
-	echo "note : use quote for TOKEN"
-	echo "ex : docker run -dit --name=container-name dc-music-bot:me 3445454345 34636566534 \"hfsbvfhg.fhwguyuie-fj\""
+	echo "error parameter kosong"
+	
 	exit 1
 fi
+
+showHelp() {
+	echo ""
+	echo "SERVER_ID APPLICATION_ID TOKEN"
+	echo "note : pakai tanda kutip untuk TOKEN"
+	echo "ex : docker run -dit --name=container-name dc-music-bot:me 3445454345 34636566534 \"hfsbvfhg.fhwguyuie-fj\""
+	echo "jika di server discord slash command tidak muncul tambah kan paramaeter \'load\' di akhir perintah"
+	echo "ex : docker run -dit --name=container-name dc-music-bot:me 3445454345 34636566534 \"hfsbvfhg.fhwguyuie-fj\" load"
+}
+
+case "$1" in
+	--help|-h|h)
+		
+	;;
+esac
+
+
+
 
 export SERVER_ID=$1
 export APPLICATION_ID=$2
