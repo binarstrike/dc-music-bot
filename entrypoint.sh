f8550f8336dc7045f92d4230a1001bd6a0d0eb2c
binarstrike <binarnugroho775@gmail.com>
2022-09-28 16:49:23 +0000

    update: menambahkan load slash command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,23 @@
-if [ -z $1 ]; then
-	echo "error parameter kosong"
-	
-	exit 1
-fi
-
 showHelp() {
-	echo ""
+	echo "--help | -h => menampilkan pesan help ini"
 	echo "SERVER_ID APPLICATION_ID TOKEN"
 	echo "note : pakai tanda kutip untuk TOKEN"
 	echo "ex : docker run -dit --name=container-name dc-music-bot:me 3445454345 34636566534 \"hfsbvfhg.fhwguyuie-fj\""
-	echo "jika di server discord slash command tidak muncul tambah kan paramaeter \'load\' di akhir perintah"
+	echo "jika di server discord slash command tidak muncul tambah kan paramaeter 'load' di akhir perintah"
 	echo "ex : docker run -dit --name=container-name dc-music-bot:me 3445454345 34636566534 \"hfsbvfhg.fhwguyuie-fj\" load"
 }
 
+if [ -z $1 ]; then
+	echo "error parameter kosong"
+	showHelp
+	exit 1
+fi
+
+
+
 case "$1" in
 	--help|-h|h)
-		
+		showHelp
 	;;
 esac
 
@@ -25,6 +27,14 @@ esac
 export SERVER_ID=$1
 export APPLICATION_ID=$2
 export TOKEN=$3
+if [ ! -z $4 ] && test $4 = "load"; then
+	export LOAD="load"
+	cd /bot
+	/bin/bash service.sh
+	unset LOAD
+	/bin/bash service.sh
 
-cd /bot
-/bin/bash service.sh
+else
+	cd /bot
+	/bin/bash service.sh
+fi
