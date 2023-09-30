065f8bc7a292a1652737ba72073fa4bc21acbd30
Binar Nugroho <binarnugroho775@gmail.com>
2023-09-30 06:09:15 +0000

    fix typo words and format code

--- slash/resume.js
+++ slash/resume.js
@@ -1,18 +1,13 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("resume")
-    .setDescription("Resumes the music"),
+  data: new SlashCommandBuilder().setName("resume").setDescription("Resumes the music"),
   run: async ({ client, interaction }) => {
     const queue = client.player.getQueue(interaction.guildId);
 
-    if (!queue)
-      return await interaction.editReply("There are no songs in the queue");
+    if (!queue) return await interaction.editReply("There are no songs in the queue");
 
     queue.setPaused(false);
-    await interaction.editReply(
-      "Music has been paused! Use `/pause` to resume the music"
-    );
+    await interaction.editReply("Music has been resumed! Use `/pause` to pause the music");
   },
 };
